34ccf45cdc4ff81122eda9bccc85a80af362555c
Ian Clarke <ian.clarke@gmail.com>
2014-04-26 10:01:43 -0500

    fix build error

--- src/main/java/quickdt/predictiveModels/decisionTree/Tree.java
+++ src/main/java/quickdt/predictiveModels/decisionTree/Tree.java
@@ -15,7 +15,7 @@ import java.io.Serializable;
  * Time: 3:15 PM
  * To change this template use File | Settings | File Templates.
  */
-class Tree implements PredictiveModel {
+public class Tree implements PredictiveModel {
     static final long serialVersionUID = 56394564395635672L;
 
     public final Node node;
