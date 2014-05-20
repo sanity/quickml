929171abc263f914fcaa225430e87cd2e261d030
Ian Clarke <ian.clarke@gmail.com>
2014-05-20 17:10:12 -0500

    test of featureEngineering framework
    [#71505794]

---
+++ src/test/java/quickdt/predictiveModels/featureEngineering/FeatureEngineeringPredictiveModelBuilderTest.java
@@ -0,0 +1,88 @@
+package quickdt.predictiveModels.featureEngineering;
+
+import com.beust.jcommander.internal.Lists;
+import org.testng.annotations.Test;
+import quickdt.data.*;
+import quickdt.predictiveModels.PredictiveModel;
+import quickdt.predictiveModels.PredictiveModelBuilder;
+
+import javax.annotation.Nullable;
+import java.io.PrintStream;
+import java.io.Serializable;
+import java.util.List;
+
+public class FeatureEngineeringPredictiveModelBuilderTest {
+
+    @Test
+    public void simpleTest() {
+        TestFEPMB testFEPMB = new TestFEPMB(new TestPMBuilder());
+        List<Instance> trainingData = Lists.newArrayList();
+        trainingData.add(new Instance(new HashMapAttributes(), 1));
+        final FeatureEngineeredPredictiveModel pm = testFEPMB.buildPredictiveModel(trainingData);
+        final Attributes emptyAttributes = trainingData.get(0).getAttributes();
+        pm.getProbability(emptyAttributes, 1);
+        pm.getClassificationByMaxProb(emptyAttributes);
+    }
+
+    public static class TestFEPMB extends FeatureEngineeringPredictiveModelBuilder {
+
+        public TestFEPMB(final PredictiveModelBuilder<?> wrappedBuilder) {
+            super(wrappedBuilder);
+        }
+
+        @Override
+        public AttributesEnricher createAttributesEnricher(final Iterable<? extends AbstractInstance> trainingData) {
+            return new AttributesEnricher() {
+                private static final long serialVersionUID = -4851048617673142530L;
+
+                @Nullable
+                @Override
+                public Attributes apply(@Nullable final Attributes attributes) {
+                    HashMapAttributes er = new HashMapAttributes();
+                    er.putAll(attributes);
+                    er.put("enriched", 1);
+                    return er;
+                }
+            };
+        }
+    }
+
+    public static class TestPMBuilder implements PredictiveModelBuilder<TestPM> {
+
+        @Override
+        public TestPM buildPredictiveModel(final Iterable<? extends AbstractInstance> trainingData) {
+            for (AbstractInstance instance : trainingData) {
+                if (!instance.getAttributes().containsKey("enriched")) {
+                    throw new IllegalArgumentException("Predictive model training data must contain enriched instances");
+                }
+            }
+
+            return new TestPM();
+        }
+    }
+
+    public static class TestPM implements PredictiveModel {
+
+        private static final long serialVersionUID = -3449746370937561259L;
+
+        @Override
+        public double getProbability(final Attributes attributes, final Serializable classification) {
+            if (!attributes.containsKey("enriched")) {
+                throw new IllegalArgumentException("Predictive model training data must contain enriched instances");
+            }
+            return 0;
+        }
+
+        @Override
+        public void dump(final PrintStream printStream) {
+
+        }
+
+        @Override
+        public Serializable getClassificationByMaxProb(final Attributes attributes) {
+            return null;
+        }
+    }
+
+}
+
