2afc5b8727a689b529c77fa5934e946b8141b445
Ian Clarke <ian.clarke@gmail.com>
2014-05-21 17:15:57 -0500

    complete, ready for code review
    [#71796632]

---
+++ src/main/java/quickdt/predictiveModels/featureEngineering/enrichStrategies/attributeCombiner/AttributeCombiningEnrichStrategy.java
@@ -0,0 +1,24 @@
+package quickdt.predictiveModels.featureEngineering.enrichStrategies.attributeCombiner;
+
+import quickdt.data.AbstractInstance;
+import quickdt.predictiveModels.featureEngineering.AttributesEnrichStrategy;
+import quickdt.predictiveModels.featureEngineering.AttributesEnricher;
+
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Created by ian on 5/21/14.
+ */
+public class AttributeCombiningEnrichStrategy implements AttributesEnrichStrategy {
+    private final Set<List<String>> attributesToCombine;
+
+    public AttributeCombiningEnrichStrategy(final Set<List<String>> attributesToCombine) {
+        this.attributesToCombine = attributesToCombine;
+    }
+
+    @Override
+    public AttributesEnricher build(final Iterable<? extends AbstractInstance> trainingData) {
+        return new AttributeCombiningEnricher(attributesToCombine);
+    }
+}
