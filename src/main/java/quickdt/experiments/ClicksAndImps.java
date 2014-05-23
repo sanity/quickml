a39b8bc14d438e81178d4c3c60bc0331371c1eb0
athawk81 <tomahawkbear@gmail.com>
2014-05-23 13:28:12 -0500

    have csvReader working

---
+++ src/main/java/quickdt/experiments/ClicksAndImps.java
@@ -0,0 +1,7 @@
+package quickdt.experiments;
+
+/**
+ * Created by alexanderhawk on 5/23/14.
+ */
+public class ClicksAndImps {
+}
--- src/main/java/quickdt/predictiveModels/downsamplingPredictiveModel/Utils.java
+++ src/main/java/quickdt/predictiveModels/downsamplingPredictiveModel/Utils.java
@@ -3,7 +3,7 @@ package quickdt.predictiveModels.downsamplingPredictiveModel;
 /**
  * Created by ian on 4/23/14.
  */
-class Utils {
+public class Utils {
     public static double correctProbability(final double dropProbability, final double uncorrectedProbability) {
         return (1.0 - dropProbability)*uncorrectedProbability / (1.0 - dropProbability * uncorrectedProbability);
     }
