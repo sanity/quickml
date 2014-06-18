cd0a282d568c597309207c84705e2b5b59a94399
Chris Reeves <reevesc@gmail.com>
2014-06-18 15:36:15 -0500

    minimium amount cross data per classification

--- src/main/java/quickdt/predictiveModels/splitOnAttributePredictiveModel/SplitOnAttributePMBuilderBuilder.java
+++ src/main/java/quickdt/predictiveModels/splitOnAttributePredictiveModel/SplitOnAttributePMBuilderBuilder.java
@@ -12,8 +12,8 @@ import java.util.Set;
  * Created by chrisreeves on 6/10/14.
  */
 public class SplitOnAttributePMBuilderBuilder implements PredictiveModelBuilderBuilder<SplitOnAttributePM, SplitOnAttributePMBuilder> {
-    private static final String MIN_AMOUNT_CROSS_DATA_TOTAL = "minAmountCrossDataTotal";
-    private static final String MIN_AMOUNT_CROSS_DATA_CLASSIFICATION = "minAmountCrossDataClassificaiton";
+    private static final String MIN_AMOUNT_TOTAL_CROSS_DATA = "minAmountTotalCrossData";
+    private static final String MIN_AMOUNT_CROSS_DATA_CLASSIFICATION = "minAmountCrossDataClassification";
     private static final String PERCENT_CROSS_DATA = "percentCrossData";
     private final PredictiveModelBuilderBuilder<?, ?> wrappedBuilderBuilder;
     private final String attributeKey;
@@ -29,7 +29,7 @@ public class SplitOnAttributePMBuilderBuilder implements PredictiveModelBuilderB
     public Map<String, FieldValueRecommender> createDefaultParametersToOptimize() {
         Map<String, FieldValueRecommender> parametersToOptimize = Maps.newHashMap();
         parametersToOptimize.putAll(wrappedBuilderBuilder.createDefaultParametersToOptimize());
-        parametersToOptimize.put(MIN_AMOUNT_CROSS_DATA_TOTAL, new FixedOrderRecommender(0, 100, 1000));
+        parametersToOptimize.put(MIN_AMOUNT_TOTAL_CROSS_DATA, new FixedOrderRecommender(0, 100, 1000));
         parametersToOptimize.put(PERCENT_CROSS_DATA, new FixedOrderRecommender(0.1, 0.2, 0.5));
         parametersToOptimize.put(MIN_AMOUNT_CROSS_DATA_CLASSIFICATION, new FixedOrderRecommender(0, 10, 100));
         return parametersToOptimize;
@@ -37,7 +37,7 @@ public class SplitOnAttributePMBuilderBuilder implements PredictiveModelBuilderB
 
     @Override
     public SplitOnAttributePMBuilder buildBuilder(final Map<String, Object> predictiveModelConfig) {
-        final long minAmountCrossData = (Long) predictiveModelConfig.get(MIN_AMOUNT_CROSS_DATA_TOTAL);
+        final long minAmountCrossData = (Long) predictiveModelConfig.get(MIN_AMOUNT_TOTAL_CROSS_DATA);
         final double percentCrossData = (Double) predictiveModelConfig.get(PERCENT_CROSS_DATA);
         final long minAmountCrossDataClassification = (Long) predictiveModelConfig.get(MIN_AMOUNT_CROSS_DATA_CLASSIFICATION);
         return new SplitOnAttributePMBuilder(attributeKey, wrappedBuilderBuilder.buildBuilder(predictiveModelConfig),
