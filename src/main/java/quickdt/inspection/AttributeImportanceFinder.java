01a967d150d1938406c07372d0fe8d5c15011524
Ian Clarke <ian.clarke@gmail.com>
2014-06-24 09:23:17 -0500

    very simple change
    [#73799554]

--- src/main/java/quickdt/inspection/AttributeImportanceFinder.java
+++ src/main/java/quickdt/inspection/AttributeImportanceFinder.java
@@ -6,6 +6,7 @@ import com.twitter.common.stats.ReservoirSampler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import quickdt.Misc;
+import quickdt.crossValidation.CrossValidator;
 import quickdt.crossValidation.StationaryCrossValidator;
 import quickdt.data.*;
 import quickdt.predictiveModels.PredictiveModel;
@@ -31,7 +32,7 @@ public class AttributeImportanceFinder {
         return determineAttributeImportance(new StationaryCrossValidator(4, 1), predictiveModelBuilder, trainingData);
     }
 
-    public TreeSet<AttributeScore> determineAttributeImportance(StationaryCrossValidator crossValidator, PredictiveModelBuilder predictiveModelBuilder, final Iterable<AbstractInstance> trainingData) {
+    public TreeSet<AttributeScore> determineAttributeImportance(CrossValidator crossValidator, PredictiveModelBuilder predictiveModelBuilder, final Iterable<AbstractInstance> trainingData) {
 
         Set<String> attributes = Sets.newHashSet();
         for (AbstractInstance instance : trainingData) {
