8b46a553560ca6c52325ec62e83145d4d19700b9
Chris Reeves <reevesc@gmail.com>
2014-08-11 14:33:29 -0500

    [#76732080] fix types of instance

--- src/main/java/quickdt/predictiveModels/UpdatablePredictiveModelBuilder.java
+++ src/main/java/quickdt/predictiveModels/UpdatablePredictiveModelBuilder.java
@@ -1,12 +1,11 @@
 package quickdt.predictiveModels;
 import quickdt.data.Instance;
 
-import java.util.List;
 
 /**
  * Created by chrisreeves on 5/22/14.
  */
 public interface UpdatablePredictiveModelBuilder<R, PM extends PredictiveModel<R, ?>> extends PredictiveModelBuilder<R, PM>{
-    public abstract void updatePredictiveModel(PM predictiveModel, Iterable<? extends Instance<R>> newData, List<? extends Instance<R>> trainingData, boolean splitNodes);
+    public abstract void updatePredictiveModel(PM predictiveModel, Iterable<? extends Instance<R>> newData, boolean splitNodes);
     public abstract void stripData(PM predictiveModel);
 }
--- src/main/java/quickdt/predictiveModels/decisionTree/tree/UpdatableLeaf.java
+++ src/main/java/quickdt/predictiveModels/decisionTree/tree/UpdatableLeaf.java
@@ -1,29 +1,31 @@
 package quickdt.predictiveModels.decisionTree.tree;
 
-import quickdt.data.AbstractInstance;
+import quickdt.data.Instance;
 
-import java.io.Serializable;
 import java.util.Collection;
 import java.util.HashSet;
-import java.util.Map;
 
 /**
  * Created by Chris on 5/14/2014.
  */
 public class UpdatableLeaf extends Leaf {
-    public final Collection<Integer> trainingDataIndexes = new HashSet<>();
+    private final Collection<Instance> instances = new HashSet<>();
 
-    public UpdatableLeaf(Node parent, Iterable<? extends AbstractInstance<Map<String, Serializable>>> instances, int depth) {
+    public UpdatableLeaf(Node parent, Iterable<Instance> instances, int depth) {
         super(parent, instances, depth);
-        for(AbstractInstance<Map<String, Serializable>> instance : instances) {
-            trainingDataIndexes.add(instance.index);
+        for(Instance instance : instances) {
+            this.instances.add(instance);
         }
     }
 
-    public void addInstance(AbstractInstance<Map<String, Serializable>> instance) {
+    public void addInstance(Instance instance) {
         classificationCounts.addClassification(instance.getLabel(), instance.getWeight());
-        trainingDataIndexes.add(instance.index);
+        instances.add(instance);
         exampleCount++;
+    }
+
+    public Collection<Instance> getInstances() {
+        return instances;
     }
 
     @Override
@@ -34,7 +36,7 @@ public class UpdatableLeaf extends Leaf {
 
         UpdatableLeaf that = (UpdatableLeaf) o;
 
-        if (!trainingDataIndexes.equals(that.trainingDataIndexes))
+        if (!instances.equals(that.instances))
             return false;
 
         return true;
@@ -43,7 +45,7 @@ public class UpdatableLeaf extends Leaf {
     @Override
     public int hashCode() {
         int result = super.hashCode();
-        result = 31 * result + trainingDataIndexes.hashCode();
+        result = 31 * result + instances.hashCode();
         return result;
     }
 }
