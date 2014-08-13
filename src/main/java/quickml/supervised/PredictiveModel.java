8e8b34ac15b209379203551db9e81f0d8c806dbe
Ian Clarke <ian.clarke@gmail.com>
2014-08-13 12:57:42 -0500

    sholdn't have removed this

--- src/main/java/quickml/supervised/PredictiveModel.java
+++ src/main/java/quickml/supervised/PredictiveModel.java
@@ -3,13 +3,14 @@ package quickml.supervised;
 import quickml.data.Instance;
 import quickml.supervised.crossValidation.crossValLossFunctions.LabelPredictionWeight;
 
+import java.io.Serializable;
 import java.util.List;
 
 /**
  * A predictive model, typically created by a supervised learning algorithm.
  * Given a set of regressors, will generate a prediction.
  */
-public interface PredictiveModel<R, P> {
+public interface PredictiveModel<R, P> extends Serializable {
 
     P predict(R regressors);
     void dump(Appendable appendable);
