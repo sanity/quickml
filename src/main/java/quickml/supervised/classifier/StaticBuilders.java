c875e8e95e946a84128f141afb22e449cbb1a024
athawk81 <tomahawkbear@gmail.com>
2015-03-09 17:24:44 -0500

    added flexibility to pass in a PredictiveModelBuilder to a StaticBuilder method

--- src/main/java/quickml/supervised/classifier/StaticBuilders.java
+++ src/main/java/quickml/supervised/classifier/StaticBuilders.java
@@ -48,7 +48,7 @@ import static quickml.supervised.classifier.randomForest.RandomForestBuilder.NUM
 public class StaticBuilders {
     private static final Logger logger = LoggerFactory.getLogger(StaticBuilders.class);
 
-    public static <T extends ClassifierInstance> Pair<Map<String, Object>, DownsamplingClassifier>  getOptimizedDownsampledRandomForest(List<T> trainingData, int rebuildsPerValidation, double fractionOfDataForValidation, ClassifierLossFunction lossFunction, DateTimeExtractor dateTimeExtractor,  Map<String, FieldValueRecommender> config) {
+    public static <T extends ClassifierInstance> Pair<Map<String, Object>, DownsamplingClassifier>  getOptimizedDownsampledRandomForest(List<T> trainingData, int rebuildsPerValidation, double fractionOfDataForValidation, ClassifierLossFunction lossFunction, DateTimeExtractor dateTimeExtractor, DownsamplingClassifierBuilder<ClassifierInstance> modelBuilder,  Map<String, FieldValueRecommender> config) {
         /**
          * @param rebuildsPerValidation is the number of times the model will be rebuilt with a new training set while estimating the loss of a model
          *                              with a prarticular set of hyperparameters
@@ -60,8 +60,9 @@ public class StaticBuilders {
         double crossValidationFraction = 0.2;
         TrainingDataCycler<T> outOfTimeData = new OutOfTimeData<T>(trainingData, crossValidationFraction, timeSliceHours, dateTimeExtractor);
         ClassifierLossChecker<T> classifierInstanceClassifierLossChecker = new ClassifierLossChecker<>(lossFunction);
+
         PredictiveModelOptimizer optimizer=  new PredictiveModelOptimizerBuilder<Classifier, T>()
-                        .modelBuilder(new RandomForestBuilder<>())
+                        .modelBuilder(modelBuilder)
                         .dataCycler(outOfTimeData)
                         .lossChecker(classifierInstanceClassifierLossChecker)
                         .valuesToTest(config)
@@ -75,7 +76,13 @@ public class StaticBuilders {
         DownsamplingClassifier downsamplingClassifier = downsamplingClassifierBuilder.buildPredictiveModel(trainingData);
         return new Pair<Map<String, Object>, DownsamplingClassifier>(bestParams, downsamplingClassifier);
     }
-    public static Pair<Map<String, Object>, DownsamplingClassifier> getOptimizedDownsampledRandomForest(List<? extends ClassifierInstance> trainingData,  int rebuildsPerValidation, double fractionOfDataForValidation, ClassifierLossFunction lossFunction, DateTimeExtractor dateTimeExtractor) {
+
+    public static <T extends ClassifierInstance> Pair<Map<String, Object>, DownsamplingClassifier>  getOptimizedDownsampledRandomForest(List<T> trainingData, int rebuildsPerValidation, double fractionOfDataForValidation, ClassifierLossFunction lossFunction, DateTimeExtractor dateTimeExtractor,  Map<String, FieldValueRecommender> config) {
+        DownsamplingClassifierBuilder<ClassifierInstance> modelBuilder = new DownsamplingClassifierBuilder<>(new RandomForestBuilder<>(), .1);
+        return getOptimizedDownsampledRandomForest(trainingData,  rebuildsPerValidation, fractionOfDataForValidation, lossFunction, dateTimeExtractor, modelBuilder, config);
+
+    }
+        public static Pair<Map<String, Object>, DownsamplingClassifier> getOptimizedDownsampledRandomForest(List<? extends ClassifierInstance> trainingData,  int rebuildsPerValidation, double fractionOfDataForValidation, ClassifierLossFunction lossFunction, DateTimeExtractor dateTimeExtractor) {
         Map<String, FieldValueRecommender> config = createConfig();
         return getOptimizedDownsampledRandomForest(trainingData,  rebuildsPerValidation, fractionOfDataForValidation, lossFunction, dateTimeExtractor, config);
     }
