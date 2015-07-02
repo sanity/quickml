fd8943c23bd9ffea088ab86e37bb1e5a140b1a28
athawk81 <tomahawkbear@gmail.com>
2015-07-01 19:31:00 -0500

    compiles and runs...but predictive accuracy questionable

---
+++ src/test/java/quickml/supervised/crossValidation/lossfunctions/LossFunctionsTest.java
@@ -0,0 +1,12 @@
+package quickml.supervised.crossValidation.lossfunctions;
+
+import org.junit.Test;
+
+public class LossFunctionsTest {
+
+
+    @Test
+    public void testName() throws Exception {
+        System.out.println(1 - 0.234);
+    }
+}
