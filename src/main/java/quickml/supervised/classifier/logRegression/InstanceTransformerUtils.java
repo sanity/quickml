dad35ff2cd22cc675cc5a0067eb5e231a6a1807b
Chris Reeves <reevesc@gmail.com>
2015-10-14 15:17:28 -0500

    Function to count attribute products in a set of training instances

--- src/main/java/quickml/supervised/classifier/logRegression/InstanceTransformerUtils.java
+++ src/main/java/quickml/supervised/classifier/logRegression/InstanceTransformerUtils.java
@@ -1,9 +1,12 @@
 package quickml.supervised.classifier.logRegression;
 
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import quickml.data.ClassifierInstance;
+import quickml.data.InstanceWithAttributesMap;
 
 import java.io.Serializable;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -12,6 +15,34 @@ import java.util.Map;
  * Created by alexanderhawk on 10/12/15.
  */
 public class InstanceTransformerUtils {
+
+    public static Map<String, Integer> getAttributeProductCounts(List<? extends InstanceWithAttributesMap> trainingData) {
+        Map<String, Integer> map = new HashMap<>();
+        for (InstanceWithAttributesMap instance : trainingData) {
+            List<String> keys = Lists.newArrayList(instance.getAttributes().keySet());
+            Collections.sort(keys);
+            for (int i = 0; i < keys.size() - 1; i++) {
+                String firstKey = keys.get(i);
+                if (instance.getAttributes().get(firstKey).equals(0.0)) {
+                    continue;
+                }
+                for (int j = i + 1; j < keys.size(); j++) {
+                    String secondKey = keys.get(j);
+                    if (instance.getAttributes().get(secondKey).equals(0.0)) {
+                        continue;
+                    }
+                    String key = firstKey + "-" + secondKey;
+                    Integer count = map.get(key);
+                    if (count == null) {
+                        count = 0;
+                    }
+                    count++;
+                    map.put(key, count);
+                }
+            }
+        }
+        return map;
+    }
 
     public static <T extends ClassifierInstance> HashMap<String, Integer> populateNameToIndexMap(List<T> trainingData) {
         HashMap<String, Integer> nameToIndexMap = Maps.newHashMap();
@@ -47,7 +78,8 @@ public class InstanceTransformerUtils {
 
     private static <T extends ClassifierInstance> boolean hasOneZeroLabels(List<T> trainingData) {
         for (T sparseClassifierInstance : trainingData) {
-            if (!sparseClassifierInstance.getLabel().equals(Double.valueOf(1.0)) && !sparseClassifierInstance.getLabel().equals(Double.valueOf(0.0))) {
+            if (!sparseClassifierInstance.getLabel().equals(Double.valueOf(1.0)) &&
+                    !sparseClassifierInstance.getLabel().equals(Double.valueOf(0.0))) {
                 return false;
             }
         }
